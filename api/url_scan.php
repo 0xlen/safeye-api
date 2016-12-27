93cbca7451da81bbe9399b50aa2e78da45d0e0d3
Len <a78965@gmail.com>
2016-12-27 19:58:38 +0800

    Update with options and url cache

--- api/url_scan.php
+++ api/url_scan.php
@@ -1,6 +1,28 @@
 <?php
 include_once '../api.php';
+include_once '../cache.php';
+
+$options = [];
 
 $scan_url = $_POST['url'];
-$url_scanner = new API('/url/report', ['resource' => $scan_url]);
-$url_scanner->outputJSON();
+$options = $_POST['options'];
+$data = ['resource' => $scan_url];
+
+$cache = getJsonCache(true);
+if (isset($cache[$scan_url])) {
+    header("Access-Control-Allow-Origin: *");
+    header('Content-type:application/json;charset=utf-8');
+
+    echo json_encode(array_merge($cache[$scan_url], ['cache' => 1]));
+} else {
+    $data = array_merge($data, (array)$options);
+
+    $url_scanner = new API('/url/report', $data);
+    $url_scanner->outputJSON();
+
+    try {
+        saveCache(json_decode($url_scanner->getResult(), true));
+    } catch (\Exception $e) {
+        echo $e->getCode() . ':' . $e->getMessage();
+    }
+}
